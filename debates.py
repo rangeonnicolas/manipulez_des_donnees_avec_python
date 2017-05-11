84c9becbb2b9999cd3168d3237ef5e98d6b5c8a0
Céline MS <celine.martinet.sanchez+github@gmail.com>
2017-05-11 13:21:04 +0200

    .

--- debates.py
+++ debates.py
@@ -1,33 +1,28 @@
-if not NOTEBOOK:     ## todo: penser à enlever
-    import mymodule
+import argparse
+import analysis as an
+import logging as lg
 
-    
 def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-d","--datafile",help="""XML file containing the scripts 
+        of the debates""")
+    parser.add_argument("-l","--lastndays",help="""limits the search to the debates
+        dating from less than --lastndays days before today, or before the date 
+        specified with --enddate""")
+    parser.add_argument("-e","--enddate",help="""limits the search to the debates 
+        that occured before --enddate. Please give the date with this format : '%Y-%m-%d' .""")
     
-    if not NOTEBOOK:
-        parser = argparse.ArgumentParser()
-        
-        parser.add_argument("-d","--datafile",help="""XML file containing the scripts 
-            of the debates""")
-        parser.add_argument("-l","--lastndays",help="""limits the search to the debates
-            dating from less than --lastndays days before today, or before the date 
-            specified with --enddate""")
-        parser.add_argument("-e","--enddate",help="""limits the search to the debates 
-            that occured before --enddate. Please give the date with this format : '%Y-%m-%d' .""")
-        
-        return parser.parse_args()
-    
-    else: ## todo: A enlever. Ici on triche car ArgumentParser est innutilisable dans un notebook
-        class foo:
-            pass
-        o = foo()
-        o.__dict__ = {
-            'datafile' : "SyceronBrut.xml",
-            'lastndays' : 365,
-            'enddate' : "2017-02-01"
-        }
-        return o
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()    
+    an.launch_analysis(args.datafile, args.lastndays, args.enddate) ##todo: faire du packing/unpacking pour tous ces arguments?
     
 if __name__ == '__main__':
-    args = parse_arguments()    
-    search(args.datafile, args.lastndays, args.enddate)
+    try:
+        main()
+    except:
+        lg.critical('You should use a flag.')
+    finally:
+        print('------------------ End of analysis -----------------')
